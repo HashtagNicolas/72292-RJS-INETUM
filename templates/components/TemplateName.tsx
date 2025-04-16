2bb5e43835d908ef13b647e62fcb048543d37370
Your Name <renaud.dubuis@decryptage.net>
2025-04-16 14:20:48 +0200

    Added props matching function

--- templates/components/TemplateName.tsx
+++ templates/components/TemplateName.tsx
@@ -8,11 +8,20 @@ interface TemplateNameProps {}
  * @example
  * <TemplateName /> 
  */
-const Test: FC<TemplateNameProps> = () => (
+const TemplateName: FC<TemplateNameProps> = () => (
  <TemplateNameWrapper data-testid="TemplateName">
     TemplateName Component
  </TemplateNameWrapper>
 );
 
+const TemplateNameMemo = React.memo(TemplateName, (prevProps,nextProps) => {
+   /*
+   Compare props to prevent unnecessary re-renders
+   return true if props are equal
+   return false if props are not equal
+   */
+   return true;
+});
+TemplateNameMemo.displayName = 'TemplateName Memoized';
 
-export default TemplateName;
+export default TemplateNameMemo;
