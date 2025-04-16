0948000cf008033988f19cdc2da9df996bde5b41
Your Name <renaud.dubuis@decryptage.net>
2025-04-16 15:37:29 +0200

    Update View Home

--- src/views/ViewHome/ViewHome.tsx
+++ src/views/ViewHome/ViewHome.tsx
@@ -1,11 +1,15 @@
 import  { FC } from 'react';
 import './ViewHome.style.css';
+import { BaseButton, BaseFooter, BaseHeader } from '../../components';
 
 interface ViewHomeProps {}
 
 const ViewHome: FC<ViewHomeProps> = () => (
  <div className="ViewHome" data-testid="ViewHome">
+    <BaseHeader />
+    <BaseButton />
     ViewHome Component
+    <BaseFooter />
  </div>
 );
 
