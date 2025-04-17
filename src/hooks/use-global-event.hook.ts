2119ace54d2bdab3c9de46f16f0aedcdbc24a5d3
Your Name <renaud.dubuis@decryptage.net>
2025-04-17 14:12:23 +0200

    2. Composables Hooks Correction

--- src/hooks/use-global-event.hook.ts
+++ src/hooks/use-global-event.hook.ts
@@ -4,3 +4,23 @@
  * @param eventName - Le nom de l'événement à écouter (ex: 'resize', 'scroll', etc.).
  * @param callback - La fonction à exécuter lorsque l'événement est déclenché.
  */
+
+import { useEffect } from "react";
+
+
+export const useGlobalEvent = (eventName: string, callback: EventListener) => {
+  // 2. Utilisation de useEffect pour ajouter et supprimer l'écouteur d'événement
+  useEffect(() => {
+    // 3. Vérification si window existe
+    if (typeof window !== 'undefined') {
+      window.addEventListener(eventName, callback);
+    }
+
+    // 4. Fonction de nettoyage pour supprimer l'écouteur d'événement
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener(eventName, callback);
+      }
+    };
+  }, [eventName, callback]); // 5. Dépendances du hook
+}
