151c335d9341cb3889ddaa33f5719131026cbb54
Your Name <renaud.dubuis@decryptage.net>
2025-04-17 14:43:28 +0200

    3. Custom Hook Composability

---
+++ src/hooks/use-online-status.hook.ts
@@ -0,0 +1,6 @@
+/**
+ * 3. Custom Hook Composability
+ * This hook is designed to manage the online status of the user's browser.
+ * It uses the `navigator.onLine` property to determine the online status.
+ * It also listens to the `online` and `offline` events to update the status accordingly.
+ */
