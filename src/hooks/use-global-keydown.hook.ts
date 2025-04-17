8fd521586310654b14b893cd4f1444fed49b058f
Your Name <renaud.dubuis@decryptage.net>
2025-04-17 15:02:38 +0200

    4. Custom Hooks

---
+++ src/hooks/use-global-keydown.hook.ts
@@ -0,0 +1,8 @@
+/**
+ * 4. Custom Hooks
+ * Crée un hook pour écouter les événements de touche au niveau global dans une application React.
+ * @description A custom hook that listens for a specific keydown event and executes a callback function.
+ * @param key - The key to listen for (e.g., 'Enter', 'Escape', etc.).
+ * @param callback - The function to execute when the key is pressed.
+ * @param options - Contextual keys such as ('ctrl', 'alt', 'shift).
+ */
